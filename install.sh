4cdc3af156dc52bf2b3302878434753a854ba381
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-08 02:49:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,32 +6,39 @@ function error_exit {
     exit 1
 }
 
-# Menginstal Dependensi yang Diperlukan
-echo "Menginstal dependensi..."
-sudo apt-get install -y autoconf automake pkg-config make gcc || error_exit "Gagal menginstal dependensi"
+# Menentukan versi libpam yang akan diunduh
+LIBPAM_VERSION="1.5.1"
+LIBPAM_URL="https://github.com/linux-pam/linux-pam/releases/download/v${LIBPAM_VERSION}/pam-${LIBPAM_VERSION}.tar.xz"
 
-# Unduh Paket libpam dari GitHub
-echo "Mengunduh libpam..."
-git clone https://github.com/linux-pam/linux-pam.git || error_exit "Gagal mengunduh libpam"
+# Menginstal dependensi yang dibutuhkan
+echo "Menginstal dependensi yang dibutuhkan..."
+
+# Unduh file tar.xz dari libpam
+echo "Mengunduh libpam versi ${LIBPAM_VERSION}..."
+wget ${LIBPAM_URL} || error_exit "Gagal mengunduh libpam."
+
+# Ekstrak file tar.xz
+echo "Mengekstrak libpam..."
+tar -xvf pam-${LIBPAM_VERSION}.tar.xz || error_exit "Gagal mengekstrak libpam."
 
 # Masuk ke direktori libpam
-cd linux-pam || error_exit "Gagal masuk ke direktori libpam"
+cd pam-${LIBPAM_VERSION} || error_exit "Gagal masuk ke direktori libpam."
 
-# Menjalankan autogen.sh untuk menghasilkan file configure
-echo "Menjalankan autogen.sh..."
-./autogen.sh || error_exit "Gagal menjalankan autogen.sh"
+# Menjalankan autogen.sh untuk menghasilkan file configure (jika tidak ada configure)
+echo "Menjalankan autogen.sh untuk menghasilkan configure..."
+./autogen.sh || error_exit "Gagal menjalankan autogen.sh."
 
 # Mengonfigurasi libpam
 echo "Mengonfigurasi libpam..."
-./configure --prefix=/usr --sysconfdir=/etc || error_exit "Gagal mengonfigurasi libpam"
+./configure --prefix=/usr --sysconfdir=/etc || error_exit "Gagal mengonfigurasi libpam."
 
 # Mengompilasi libpam
 echo "Mengompilasi libpam..."
-make || error_exit "Gagal mengompilasi libpam"
+make || error_exit "Gagal mengompilasi libpam."
 
 # Menginstal libpam
 echo "Menginstal libpam..."
-sudo make install || error_exit "Gagal menginstal libpam"
+sudo make install || error_exit "Gagal menginstal libpam."
 
 # Verifikasi Instalasi
 echo "Verifikasi instalasi libpam..."
@@ -39,6 +46,6 @@ ls /usr/lib | grep pam || error_exit "libpam tidak ditemukan di /usr/lib"
 ls /usr/include/security | grep pam || error_exit "Header libpam tidak ditemukan di /usr/include/security"
 
 # Cek versi libpam menggunakan pkg-config (jika tersedia)
-pkg-config --modversion pam || error_exit "libpam tidak ditemukan dengan pkg-config"
+pkg-config --modversion pam || error_exit "libpam tidak ditemukan dengan pkg-config."
 
 echo "Instalasi libpam-devel berhasil!"
